aa5b3a8b9d22ce31656e1691ae3068a96604a2c3
niko <test@test.com>
2016-02-04 19:33:33 +0300

    fix docker

--- cnf.sh
+++ cnf.sh
@@ -3,7 +3,7 @@
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 sudo apt-get install curl
-curl -s https://get.docker.io/ubuntu/ | sudo sh
+wget -qO- https://get.docker.com/ | sh
 
 sudo add-apt-repository ppa:fkrull/deadsnakes
 
@@ -11,7 +11,8 @@ wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo ap
 
 sudo apt-get update;
 sudo apt-get upgrade;
-sudo apt-get install vim git tree colordiff sysstat ack-grep htop skype subversion mysql-server memcached rabbitmq-server libgeoip1 libgeoip-dev libmemcached-dev zlib1g-dev libssl-dev python-dev build-essential mysql-server python-mysqldb libmysqlclient-dev python-virtualenv python-pip postgresql-9.4 postgresql-contrib terminator libpq-dev apache2-utils curl redis-server python-numpy python-scipy python-matplotlib libxft-dev 
+sudo apt-get install vim git tree colordiff sysstat ack-grep htop skype subversion mysql-server memcached rabbitmq-server libgeoip1 libgeoip-dev libmemcached-dev zlib1g-dev libssl-dev python-dev build-essential mysql-server python-mysqldb libmysqlclient-dev python-virtualenv python-pip postgresql-9.4 postgresql-contrib terminator libpq-dev apache2-utils curl redis-server 
+sudo apt-get install gfortran libblas-dev liblapack-dev python-numpy python-scipy python-matplotlib libxft-dev 
 sudo apt-get install apache2 php5 php5-mysql php5-xdebug php5-curl php5-memcache php5-mongo
 
 #store git password
